846bc5dc37a5a00a97265d4de0ccbd3251421ecb
Sergey Nevmerzhitsky <twinchic@gmail.com>
2017-02-11 01:15:01 +0300

    Add stubs for mature configuration IPs of PgSQL servers

--- README.md
+++ README.md
@@ -1,3 +1,8 @@
 Docker image only phpPgAdmin 5.0 application on latest nginx and PHP 5.6.
 
 TODO Maybe it's a good idea to switch to https://github.com/bitnami/bitnami-docker-php-fpm.
+
+# PhpPgAdmin Configuration
+
+* `dockerized-config.inc.php`
+* `external-config.inc.php`
---
+++ pgadmin/config.proxy.php
@@ -0,0 +1,9 @@
+<?php
+/**
+ * ATTENTION!!!
+ * Don't edit this config for configuration of the PgAdmin servers. Read "PhpPgAdmin Configuration"
+ * section in the README.md.
+ */
+
+// If dockerized-config.inc.php exists - use it.
+// Else read external-config.inc.php content and wrap it to Docker IPS.

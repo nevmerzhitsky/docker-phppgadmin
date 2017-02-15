dcc60424c80c81e8016de649f8d09843bf8903ef
Sergey Nevmerzhitsky <twinchic@gmail.com>
2017-02-15 22:37:27 +0300

    Remove PGADMIN_CONFIG_MODE env var Rewrite config.proxy.php implementation

--- README.md
+++ README.md
@@ -4,35 +4,15 @@ TODO Maybe it's a good idea to switch to https://github.com/bitnami/bitnami-dock
 
 # PhpPgAdmin Configuration
 
-You can use two strategies for setup IPs/addresses of the PostgreSQL servers in PhpPgAdmin config.
-You should select the one.
-
-## Docker network
-
-If you can setup IPs/addresses strictly in the docker network (a bridge network usually), you should
-mount it to the container as file with name `docker-network-config.inc.php`. For this just add to you
-`.env` file:
-* PGADMIN_CONFIG_MODE=docker
-* PGADMIN_CONFIG_PATH=/c/Users/admin/config.pgsql.php
-
-## External network
-
-Else if you want setup config via IPs/addresses from external to the container network (a host system
-network usually) you should mount it to the container as file with name `external-network-config.inc.php`.
-For this just add to you `.env` file:
-* PGADMIN_CONFIG_MODE=external
-* PGADMIN_CONFIG_PATH=/c/Users/admin/config.pgsql.php
-
-In this case the special config proxy in the container will map external addresses to a docker network,
-but fow this you also required to add more environment variables to `.env` file:
+Because the bridge network driver of Docker is used, if a PostgreSQL server is hosted on 127.0.0.1 IP
+or the same you should make additional configuration to map these IPs to the IP of the Docker Host(s).
+Use next two ENV variables to setup this mapping:
 * PG_SERVERS_DOCKER_IPS=docker_ip1,docker_ip2,...
 * PG_SERVERS_EXTERNAL_ADDRS=external_ip1,external_ip2,...
 
-This mapping usually required when you want connect from the container to the PostgreSQL server which
-runs on the host machine at 127.0.0.1 IP, but the container network doesn't see the host machine at
-this IP. Thus you define via `PG_SERVERS_DOCKER_IPS` list of the IPs of required hosts from the
-docker network and define via `PG_SERVERS_EXTERNAL_ADDRS` list of the addresses
+You define the list of IPs of required hosts from the docker network via `PG_SERVERS_DOCKER_IPS`
+and define the list of the addresses via `PG_SERVERS_EXTERNAL_ADDRS`.
 
-For example:...
-* PG_SERVERS_DOCKER_IPS=172.0.0.1,172.0.1.1
+For example:
+* PG_SERVERS_DOCKER_IPS=172.20.0.1,172.18.0.2
 * PG_SERVERS_EXTERNAL_ADDRS=127.0.0.1,127.0.0.2
--- pgadmin/config.proxy.php
+++ pgadmin/config.proxy.php
@@ -1,63 +1,62 @@
 <?php
 /**
  * ATTENTION!!!
- * Don't edit this config for configuration of the PgAdmin servers. Read "PhpPgAdmin Configuration"
+ *
+ * Don't edit this file for configuration of the PgAdmin servers. Read "PhpPgAdmin Configuration"
  * section in the README.md.
  */
 
-/**
- * @param string $value
- * @param string $oldAddress
- * @param string $newAddres
- * @return string
- */
-$serverMapper = function($value, $oldAddress, $newAddres) {
-    $parts = explode(':', $value, 2);
-    $parsedAddress = $parts[0];
+if (!file_exists(__DIR__ . '/external-config.inc.php')) {
+    die('The conf directory should contain a config with "docker-network" or "external-network" ' .
+        'prefix. Check the PhpPgAdmin Configuration section in the README');
+}
 
-    if ($parsedAddress != $oldAddress) {
-        return $value;
-    }
+require __DIR__ . '/external-config.inc.php';
 
-    return $newAddres . ($parts[1] ? ":{$parts[1]}" : '');
-};
+if (isset($_ENV['PG_SERVERS_DOCKER_IPS']) && isset($_ENV['PG_SERVERS_EXTERNAL_ADDRS'])) {
+    /**
+     * @param string $value
+     * @param string $oldAddress
+     * @param string $newAddres
+     * @return string
+     */
+    $serverMapper = function($value, $oldAddress, $newAddress) {
+        $parts = explode(':', $value, 2);
+        $parsedAddress = $parts[0];
 
-if (file_exists(__DIR__ . '/docker-network-config.inc.php')) {
-    require __DIR__ . '/docker-network-config.inc.php';
-} elseif (file_exists(__DIR__ . '/external-network-config.inc.php')) {
-    require __DIR__ . '/external-network-config.inc.php';
-
-    if (!isset($_ENV['PG_SERVERS_DOCKER_IPS']) || !isset($_ENV['PG_SERVERS_EXTERNAL_ADDRS'])) {
-        die('You should deliver "PG_SERVERS_DOCKER_IPS" and "PG_SERVERS_EXTERNAL_ADDRS" ENV ' +
-            'variables to the container. Check the PhpPgAdmin Configuration section in the README');
-    }
-
-    $dockerIps = explode(',', $_ENV['PG_SERVERS_DOCKER_IPS']);
-    $externalIps = explode(',', $_ENV['PG_SERVERS_EXTERNAL_ADDRS']);
-    $dockerIps = array_map('trim', $dockerIps);
-    $externalIps = array_map('trim', $externalIps);
-
-    if (count($dockerIps) != count($externalIps)) {
-        die('Count of elements in the "PG_SERVERS_DOCKER_IPS" and "PG_SERVERS_EXTERNAL_ADDRS" ' +
-            'ENV variables should match.');
-    }
-
-    $map = array_combine($externalIps, $dockerIps);
-    $requestArrays = array(&$_REQUEST, &$_GET, &$_POST);
-
-    foreach ($map as $extIp => $dockIp) {
-        // Map configuration
-        foreach ($conf['servers'] as &$server) {
-            $server['host'] = $serverMapper($server['host'], $extIp, $dockIp);
+        if ($parsedAddress != $oldAddress) {
+            return $value;
         }
-        // Map parameter of requests
-        foreach ($requestArrays as &$array) {
-            if (isset($array['server'])) {
-                $array['server'] = $serverMapper($array['server'], $extIp, $dockIp);
+
+        return $newAddress . ($parts[1] ? ":{$parts[1]}" : '');
+    };
+    $ipsMapper = function (&$conf) use ($serverMapper) {
+        $dockerIps = explode(',', $_ENV['PG_SERVERS_DOCKER_IPS']);
+        $dockerIps = array_map('trim', $dockerIps);
+        $externalIps = explode(',', $_ENV['PG_SERVERS_EXTERNAL_ADDRS']);
+        $externalIps = array_map('trim', $externalIps);
+
+        if (count($dockerIps) != count($externalIps)) {
+            die('Count of elements in the "PG_SERVERS_DOCKER_IPS" and "PG_SERVERS_EXTERNAL_ADDRS" ' .
+                'ENV variables should match.');
+        }
+
+        $map = array_combine($externalIps, $dockerIps);
+        $requestArrays = array(&$_REQUEST, &$_GET, &$_POST);
+
+        foreach ($map as $extIp => $dockIp) {
+            // Map configuration
+            foreach ($conf['servers'] as &$server) {
+                $server['host'] = $serverMapper($server['host'], $extIp, $dockIp);
+            }
+            // Map parameter of requests
+            foreach ($requestArrays as &$array) {
+                if (isset($array['server'])) {
+                    $array['server'] = $serverMapper($array['server'], $extIp, $dockIp);
+                }
             }
         }
-    }
-} else {
-    die('The conf directory should contain a config with "docker-network" or "external-network" ' +
-        'prefix. Check the PhpPgAdmin Configuration section in the README');
+    };
+    $ipsMapper($conf);
+    unset($serverMapper, $ipsMapper);
 }
